99475b48aa52f9040cb421a336ef19c9b8171b60
Your Name <f.kayiranga@alustudent.com>
2026-09-10 00:01:51 +0200

    setting up vite react

---
+++ src/MyComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MyComponent.css';
+
+const MyComponent: React.FC = () => {
+  return (
+    <div className="my-component">
+      <h1>Welcome to My React App</h1>
+      <p>This is a simple component built with React and TypeScript.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
