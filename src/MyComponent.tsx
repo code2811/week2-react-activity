6bf601f752d19d55416a7b48e81bc7609267c917
Your Name <f.kayiranga@alustudent.com>
2026-09-10 15:16:09 +0200

    adding component

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,12 +1,13 @@
 // MyComponent: displays a heading and paragraph
+
 import React from 'react';
 import './MyComponent.css';
 
 const MyComponent: React.FC = () => {
   return (
     <div className="my-component">
-      <h1>Welcome to My React App</h1>
-      <p>This is a simple component built with React and TypeScript.</p>
+      <h1>Hello from MyComponent!</h1>
+      <p>This is a paragraph of text within my component.</p>
     </div>
   );
 };
